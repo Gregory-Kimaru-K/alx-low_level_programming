ed499a4327374b2d8100994704600a798c48d6c7
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-16 10:19:40 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
-# include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * returns: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 # include <stdio.h>
-
+/**
+ * Has a function main
+ * has a printf containing some string
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
