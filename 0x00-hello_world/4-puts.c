05cf7316ff8c84c108a5622d4edafb1eb882f1e9
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-16 10:30:28 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * returns: Always 0 (Success)
  */
-int main(void)/*main success*/
+int main(void)/* main success */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
