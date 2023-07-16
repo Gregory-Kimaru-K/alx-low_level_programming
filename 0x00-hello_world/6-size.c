05e7795abb862bc12298fbd111add99c63caf448
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-16 10:02:15 -0700

    all at once

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void)
+{
+	printf("size of character %lu byte(s)\n", sizeof(char));
+	printf("size of character %lu byte(s)\n", sizeof(int));
+	printf("size of character %lu byte(s)\n", sizeof(long int));
+	printf("size of character %lu byte(s)\n", sizeof(long long int));
+	printf("size of character %lu byte(s)\n", sizeof(float));
+}
