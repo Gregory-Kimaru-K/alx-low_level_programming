65b4f897d31c0c0c5b5f25e13df3034974c92b00
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-19 15:36:56 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - while loop printing alphabets
+ * Return: returns 0
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
