1b31a526236e1ad64dcf67c5cf20191e996519ef
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-21 15:23:54 -0700

    all at once get ready

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - check if it is uppercase or lowercase
+ * Return:1 if upper 0 if lower
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - muliplies two digits
+ * @a: digit 1
+ * @b: digit two
+ * Return: returns 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
