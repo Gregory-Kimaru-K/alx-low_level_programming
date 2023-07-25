eb7b128430c1dc66475227afe74a4f9ae69d7d2b
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-25 15:27:45 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - prints length of a string
+ * @s: the string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
