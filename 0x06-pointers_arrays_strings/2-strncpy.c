f708046cd7ee1931959639ff69097dd722fb4160
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-28 14:32:08 -0700

    all

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string 
+ * @dest: string copy
+ * @src: string original
+ * @n: number of characters to cp
+ * Return: returns dest
+ */
+char _strncpy(char *dest, *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; i < j; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
