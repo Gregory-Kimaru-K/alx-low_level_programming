08f1f2594a605ccd4f6c5c0144fbab32ffa86908
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-28 13:48:40 -0700

    2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatinates two strings of n length line 2
+ * @dest: string 1
+ * @src: string 2
+ * @n: int of seperation
+ * Return: The string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int j, i;
+
+	i = 0;
+
+	j = 0;
+
+	while (dest[i] = '\0')
+	{
+		i++;
+	}
+	while (src[j] = '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+/**
+ * _putchar - prints characters 
+ * Return: returns 1 -1(fail)
+ */
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
