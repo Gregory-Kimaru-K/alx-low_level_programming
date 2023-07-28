2dfae5d079b4b8949d6ad89960e2235fc352e911
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-07-28 13:51:59 -0700

    2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int j, i;
+	int i, j;
 
 	i = 0;
 
 	j = 0;
 
-	while (dest[i] = '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[j] = '\0' && j < n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
