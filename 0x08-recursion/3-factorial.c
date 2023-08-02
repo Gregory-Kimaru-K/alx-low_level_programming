77d89ae0b76b116affb78a12faf9ed52deeeaec5
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-08-02 14:58:53 -0700

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,9 @@
  */
 int factorial(int n)
 {
-	unsigned int p;
-	p = n;
-
-	if (p <= 1)
+	if (n == 0 || n == 1)
 	{
 		return (1);
 	}
-	return (p * factorial(p - 1));
+	return (n * factorial(n - 1));
 }
