0c396ead3733087c40d6b79525e3c0e3472d1d7c
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-08-08 03:54:01 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print it name
+ * @argc: int rep number of arguments
+ * @argv: str listing the arguments
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
