834310620bb40ba51cd179ebde3bc3d9d3b06efe
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-08-09 10:58:21 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,21 +8,24 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *ptr;
+	char *ptr = NULL;
 
 	if (size == 0)
 	{
 		return ('\0');
 	}
 
-	else if (size != 0)
+	if (size != 0)
 	{
 
 		ptr = (char *) malloc(size * sizeof(char));
 
-		for (int i = 0; i < size; i++)
+		if (ptr != NULL)
 		{
-			ptr[i] = c;
+			for (int i = 0; i < size; i++)
+			{
+				ptr[i] = c;
+			}
 		}
 	}
 
