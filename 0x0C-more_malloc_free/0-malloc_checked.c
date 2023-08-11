3cd8db34dc973897084847d7a4ead926124a94ae
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-08-11 13:57:58 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: memory size
+ * Return: returns void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *_alocate = malloc(b);
+
+	if (alocate == NULL)
+		exit(98);
+
+	return (alocate);
+}
