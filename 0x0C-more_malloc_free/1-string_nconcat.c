64a792209eea0e868fca26bdfa02d737d98acbc5
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-08-11 14:24:31 -0700

    _calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * string_nconcat - concatinates two strings up to 
+ * string_nconcat - concatinates two strings up to
  * n number of characters
  * @s1: string 1
  * @s2: string 2
  * @n: number of character to print
+ * Return: Pointer to the cocatinated string
+ * or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
