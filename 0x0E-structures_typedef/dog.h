3746fbb14be15544d465dc2de712a3b984140a71
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-08-15 06:19:42 -0700

    Took me one hour to try and figure out why my code wasn't working turns out it was an n

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
-#ifdef DOG_H
+#ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 /**
  * struct dog - the structure
  * @name: name of the dog
  * @age: age of dog
  * @owner: owner of dog
+ * Description: This is a structure that defines the attributes of a dog
+ *
  */
 struct dog
 {
@@ -15,5 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
